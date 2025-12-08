f8e45b1b088b9d37eb51cabbcc7960ff28abc8f9
AoI40104 <aoi.ohata2434@gmail.com>
2025-12-08 16:49:11 +0900

    socket_type add

--- util/packet_type.h
+++ util/packet_type.h
@@ -3,5 +3,11 @@
 enum PacketType
 {
     PACKET_TYPE_NONE,
-    PACKET_TYPE_GAME_PHASE,
+    PACKET_TYPE_SET_PLAYER_ID, // プレイヤーID設定
+    PACKET_TYPE_GAME_PHASE,    // ゲームフェーズ
+    PACKET_TYPE_BALL_STATE,    // ボール位置
+    PACKET_TYPE_PLAYER_INPUT,  // プレイヤー操作
+    PACKET_TYPE_SCORE_UPDATE,  // スコア
+    PACKET_TYPE_JUDGE_RESULT   // 判定
+    
 };
