51329ba769a96d68234b65da62c66236f7cca050
AoI40104 <aoi.ohata2434@gmail.com>
2025-12-17 15:54:57 +0900

    ラリーに必要な情報を追加

--- ball.h
+++ ball.h
@@ -2,9 +2,13 @@
 
 #include "util/point_3d.h"
 
+
 struct Ball
 {
     Point3d point;
     Point3d velocity;
     int angle;
+
+    int Last_hit_player_id;
+    int bounce_count;
 };
--- util/packet_type.h
+++ util/packet_type.h
@@ -8,6 +8,7 @@ enum PacketType
     PACKET_TYPE_BALL_STATE,    // ボール位置
     PACKET_TYPE_PLAYER_INPUT,  // プレイヤー操作
     PACKET_TYPE_SCORE_UPDATE,  // スコア
-    PACKET_TYPE_JUDGE_RESULT   // 判定
-    
+    PACKET_TYPE_JUDGE_RESULT,   // 判定
+    PACKET_TYPE_PLAYER_STATE,  // プレイヤー位置
+    PACKET_TYPE_PLAYER_STATE  // プレイヤー位置同期用
 };
