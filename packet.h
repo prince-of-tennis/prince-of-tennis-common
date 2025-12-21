94b5339d2621c2512315d6a9414dc43bc2dc8431
rinngo0302 <rinngoyuki03020302@gmail.com>
2025-12-21 17:56:26 +0900

    iroiro

--- packet.h
+++ packet.h
@@ -2,16 +2,16 @@
 
 #include <cstdint>
 
-#include "util/packet_type.h"
 #include "ball.h"
+#include "util/packet_type.h"
 
-#define PACKET_MAX_SIZE 1024
+#define PACKET_MAX_SIZE 4096
 
 // ネットワーク通信用パケット構造体
 // 構造体パディングを防ぐため、固定サイズの型を使用
 struct Packet
 {
-    uint32_t type;  // PacketType を uint32_t として送信（エンディアン問題を回避）
+    PacketType type;
     uint32_t size;  // size_t ではなく uint32_t を使用（プラットフォーム非依存）
     uint8_t data[PACKET_MAX_SIZE];
 } __attribute__((packed));  // パディングを無効化（GCC/Clang）
--- player_input.h
+++ player_input.h
@@ -2,9 +2,10 @@
 
 struct PlayerInput
 {
+    int player_id;  // プレイヤーID (0 or 1)
     bool right;
     bool left;
     bool front;
     bool back;
     bool swing;
-};
+} __attribute__((packed));
