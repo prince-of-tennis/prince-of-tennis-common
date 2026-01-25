5bac87881d024396e8ae4bea534b1e61cf848b10
rinngo0302 <rinngoyuki03020302@gmail.com>
2026-01-25 14:17:54 +0900

    feat: gamescore

--- GameScore.h
+++ GameScore.h
@@ -1,9 +1,9 @@
 #pragma once
-#define MAX_SETS 3 
+#define MAX_SETS 5  // 5セットマッチ（3セット先取で勝利）
 struct GameScore
 {
     int games_in_set[MAX_SETS][2]; // [セット番号][プレイヤー番号 (0 or 1)]
-    int current_set;               // 現在のセット番号 (0, 1, 2)
+    int current_set;               // 現在のセット番号 (0-4)
     int current_game_p1;           // 現在のゲームでのP1のポイント数 (0, 15, 30, 40...)
     int current_game_p2;           // 現在のゲームでのP2のポイント数
 };
